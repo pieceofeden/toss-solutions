fc3c7b9c4eb4967c7746aa6bcdf1149df63741e4
pieceofeden <ashishranjan079756@gmail.com>
2019-10-13 22:10:30 +0530

    Assignment 2, Solution 2

---
+++ spl_nums.py
@@ -0,0 +1,25 @@
+num = input('Enter number: ')
+lst = []
+for digit in num:
+    lst.append(int(digit))
+num = int(num)
+flag=0
+i=1
+while(i<=num):
+    total=0
+    count = i
+    for item in lst:
+        total = total + item**i
+        if(total == num or total%num==0):
+            print("Yes! This number is special")
+            flag=1
+            break
+        else:
+            i+=1
+    if(flag==1):
+        break
+    else:
+        count+=1
+        i=count
+if(flag==0):
+    print("Nope, this isnt")
