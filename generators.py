ada88d4c034a8ad4d588b9c4fb1d807e02b8938c
pieceofeden <ashishranjan079756@gmail.com>
2019-10-14 23:18:22 +0530

    Updated Solution 4

--- generators.py
+++ generators.py
@@ -1,8 +1,10 @@
-start = int(input(''))
-end = int(input(''))
-power = int(input(''))
-def generator(start,end,power):
-    while(start <= end):
-        print(start**power)
+start = int(input())
+count = int(input())
+n = int(input())
+def generator(start,count,n):
+    i=0;
+    while(start<=count):
+        print(n**i)
+        i+=1
         start+=1
-generator(start,end,power)
+generator(start,count,n)
