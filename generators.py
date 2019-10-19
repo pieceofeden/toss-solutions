dff8fc9fad2aa139eb7f387e99be1aa7d5d95413
pieceofeden <42468504+pieceofeden@users.noreply.github.com>
2019-10-20 00:45:23 +0530

    Updated generators.py
    Updated the solution as per the problem on the hackerrank contest.

--- generators.py
+++ generators.py
@@ -1,10 +1,14 @@
-start = int(input())
-count = int(input())
-n = int(input())
+lst = list(map(int, input().split()))
+start = lst[0]
+count = lst[1]
+n = lst[2]
 def generator(start,count,n):
     i=0;
-    while(start<=count):
-        print(n**i)
+    ans=[]
+    ans.append(start)
+    for i in range(1,count):
+        ans.append(start*(n**i))
         i+=1
-        start+=1
+    for _ in ans:
+        print(_, end=" ")
 generator(start,count,n)
