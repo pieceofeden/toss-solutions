d9135e48169da8a08421fa43373427a029ef9b1a
pieceofeden <42468504+pieceofeden@users.noreply.github.com>
2019-10-20 00:40:56 +0530

    Updated and Renamed Range.py
    Updated and renamed the solution accordingly as per the hackerrank problem.

--- Increment_func.py
+++ Increment_func.py
@@ -1,8 +1,11 @@
 start = int(input(''))
 end = int(input(''))
 step = int(input(''))
+lst = []
 def Range(start,end,step):
     while(start < end):
-        print(start)
+        lst.append(start)
         start+=step
+    for item in lst:
+        print(item, end=" ")
 Range(start,end,step)
